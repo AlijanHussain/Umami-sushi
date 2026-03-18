e7acd7e5795214be5cc5a810cd89344ad7ba3637
Djoncan <6057716@mborijnland.nl>
2026-03-18 12:28:43 +0100

    heb de pagina vactures gemaakt
    heb de vacturuur pagina gemaakt en daarvan de css aangepast

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+
+function showAlert() {
+    alert("Hartelijk bedankt voor het reserveren bij Umami Sushi! We kijken ernaar uit om je te verwelkomen en je een heerlijke ervaring te bieden. Tot snel!");
+
+}   
+
+
