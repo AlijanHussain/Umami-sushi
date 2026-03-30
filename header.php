a356d0e8992c8859630631e349f01b11097a3ec8
AlijanHussain <6054499@mborijnland.nl>
2026-03-30 14:23:10 +0200

    kleine aanpassingen gemaakt

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
     <nav>
         <ul class="nav_links" id="nav_links">
             <a href="index.php">Home</a>
-            <a href="Lunch.php">Menu</a>
-            <a href="Contacten.php">Contacten</a>
+            <a href="Lunch.html">Menu</a>
+            <a href="Contacten.html">Contacten</a>
             <a href="reserveren.php">Reserveren</a>
             <a href="Vacatures.php">Vacatures</a>
         </ul>
